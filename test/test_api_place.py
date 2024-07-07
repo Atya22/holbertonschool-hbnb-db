74c4d055cbccc79337798642fe1b0651d8facc77
Mehralizada <gulanemehrelizade97@gmail.com>
2024-07-07 19:36:58 +0000

    file created

--- test/test_api_place.py
+++ test/test_api_place.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import unittest
 import json
-from api.api_place import data_manager
-from app import app
+from api.api_place import app, data_manager
 from model.city import City
 from model.amenities import Amenities
 
